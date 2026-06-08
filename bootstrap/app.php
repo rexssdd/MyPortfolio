3ab8e860b53fab6163b8abfa08edbd7c0cb22355
rexssdd <YOUR_NOREPLY_EMAIL>
2026-06-08 17:36:27 +0800

    refactor: restructure app configuration and remove unnecessary middleware setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
